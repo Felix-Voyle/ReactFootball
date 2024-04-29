d05d7d5385b7c480bf1c58c76091e33ed085503b
Felix <felix@Felixs-MacBook-Pro.local>
2024-04-29 19:51:44 +0100

    installed react router dom and set up routes

--- football/src/App.js
+++ football/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import './App.css';
-
 import CustomNavbar from './Nav/CustomNavBar';
+import Home from './Home/Home';
+import About from './About/About';
 
 function App() {
   return (
-    <CustomNavbar/>
+    <Router>
+      <>
+        <CustomNavbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
--- football/src/Nav/CustomNavBar.js
+++ football/src/Nav/CustomNavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './Nav.module.css';
 
@@ -7,13 +8,12 @@ import { Navbar, Nav, NavbarBrand, NavbarToggle, NavbarCollapse } from 'react-bo
 function CustomNavbar() {
   return (
     <Navbar className={styles.nav} bg="dark" variant="dark" expand="lg">
-      <NavbarBrand href="#home">App Name</NavbarBrand>
+      <NavbarBrand>App Name</NavbarBrand>
       <NavbarToggle aria-controls="basic-navbar-nav" />
       <NavbarCollapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link as={Link} to="/">Home</Nav.Link>
+        <Nav.Link as={Link} to="/about">About</Nav.Link>
         </Nav>
       </NavbarCollapse>
     </Navbar>

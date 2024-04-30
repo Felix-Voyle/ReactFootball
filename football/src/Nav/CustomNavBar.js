d67cbc451752714c6536e092d57092606abfa58d
Felix <felix@Felixs-MacBook-Pro.local>
2024-04-30 18:44:42 +0100

    installed auth0 SDK and added login pop up, sign ups working successfully

--- football/src/Nav/CustomNavBar.js
+++ football/src/Nav/CustomNavBar.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import styles from './Nav.module.css';
-
 import { Navbar, Nav, NavbarBrand, NavbarToggle, NavbarCollapse } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import styles from './Nav.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function CustomNavbar() {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleLoginClick = () => {
+    loginWithRedirect();
+  };
+
   return (
     <Navbar className={styles.nav} bg="dark" variant="dark" expand="lg">
-      <NavbarBrand>App Name</NavbarBrand>
       <NavbarToggle aria-controls="basic-navbar-nav" />
+      <NavbarBrand className="mx-auto">App Name</NavbarBrand>
       <NavbarCollapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/">Home</Nav.Link>
-        <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+        </Nav>
+        <Nav>
+          <Nav.Link onClick={handleLoginClick}>Login</Nav.Link>
         </Nav>
       </NavbarCollapse>
     </Navbar>
